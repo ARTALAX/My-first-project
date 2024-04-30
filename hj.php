c5359be205916511475f11d5d8cb69a72664a2c0
Jury <urazmakin135135@gmail.com>
2024-04-30 19:46:21 +0300

    авп

--- hj.php
+++ hj.php
@@ -2,15 +2,28 @@
 <html lang="ru">
 
 <head>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="styles.css">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
 
 <body>
-	<button type="button" name="but">Кнопка</button>
-	<input type="email" name="email" class="yes" placeholder="похуй">
+	<table>
+		<thead class="thead">
+			<tr>
+				<td class="item-h">Таблица</td>
+				<td class="item-h">Тест</td>
+			</tr>
+
+		</thead>
+		<tbody>
+			<tr>
+				<td class="item">dafffffffffff</td>
+				<td class="item">neeeeeeeeeeeee</td>
+			</tr>
+		</tbody>
+	</table>
 </body>
 
 </html>
