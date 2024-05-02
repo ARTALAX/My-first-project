bf2df098b126c36db745e346db8729a943a75e82
Jury <urazmakin135135@gmail.com>
2024-05-02 13:46:15 +0300

    да

--- php-example/blocs/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class='mt-5'>Все права защищены &copy; 2023</footer>
-</div>
-</body>
-
-</html>
--- php-example/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db_server_name = "localhost";
-$db_username = "root";
-$db_pass = "";
-$db_name = "test_db";
-$conn = mysqli_connect($db_server_name, $db_username, $db_pass, $db_name);
